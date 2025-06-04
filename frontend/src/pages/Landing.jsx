383e0f7e81b7638167089050224331ae9f730694
Romeiro <theromeirofernandes@gmail.com>
2025-06-04 23:24:38 +0530

    frontend setup

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <>
+      <h1 className="font-bold text-4xl text-center">Welcome to the Landing Page</h1>
+      <p>This is the landing page of our application.</p>
+    </>
+  );
+};
+
+export default Landing;
