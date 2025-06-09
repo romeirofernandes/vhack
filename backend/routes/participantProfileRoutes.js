5f34d35818155962b03989452e9deabb3a21c683
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-06-09 17:03:34 +0530

    refactor: replace profileController and profileRoutes with participantProfileController and participantProfileRoutes

--- backend/routes/participantProfileRoutes.js
+++ backend/routes/participantProfileRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const profileController = require("../controllers/profileController");
+const profileController = require("../controllers/participantProfileController");
 const authMiddleware = require("../middleware/auth");
 
 // Get user profile
